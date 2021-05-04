68fb68824fb65c52878f7e0f43b2fa946c9664a1
shubham-shinde <shindes0321@gmail.com>
2021-05-04 16:30:35 +0530

    update

--- codechef/battleOfBrians/PasswordStrength.java
+++ codechef/battleOfBrians/PasswordStrength.java
@@ -3,32 +3,8 @@ import java.io.*;
 
 class STR_PWD {
 
-    static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int testcase = Integer.parseInt(br.readLine());
-        while(testcase-- >0){
-            String strings = br.readLine();
-            
-            char arr[] = strings.toCharArray();
-            int a = 0,b=0,c=0,d=0;
-            for(int i = 0; i< strings.length(); i++){
-                if(arr[i]>=65 && arr[i] < 91)
-                    a=1;
-                else if(arr[i]>=97 && arr[i] < 123)
-                    b=1;
-                else if(arr[i]>=48 && arr[i] < 58)
-                    c=1;
-                else if(arr[i]==33 || arr[i]==64 || arr[i]==35 || arr[i]== 36 || arr[i]== 37 || arr[i]==38)//!,@,#,$,%,&
-                    d=1;
-            }
-            System.out.println(a+b+c+d);
-        }
-
-
-    }
-
-
+	public static void main(String[] args) throws IOException {
+	}
 }
---
+++ example.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main() {
+	cout<<3<<endl<<5<<endl<<'a'<<endl<<2343.343<<endl;
+	return 0;
+}
